06e6f3b3ab7300b0c72b3a5633a57002062c4d8a
Supratim Samanta <supratim.iee23.ju2005@gmail.com>
2026-03-16 23:07:38 -0400

    A simple java project on regression

---
+++ data-science/machine-learning/deep-learning/gen-ai/projects/java/lblvd-maven-regression-prediction/src/test/java/com/example/regression/prediction/PredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.regression.prediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
