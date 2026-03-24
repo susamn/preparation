5d7a915159231dbb355bc65058d2564c6091ec71
Supratim Samanta <supratim.iee23.ju2005@gmail.com>
2026-03-24 00:47:42 -0400

    Terraform variables

---
+++ iac/terraform/variables/main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "myvpc"
+  description = "This sets the vpc name"
+}
+
+variable "sshport" {
+  type    = number
+  default = 22
+}
+
+variable "enabled" {
+  type    = bool
+  default = true
+}
+
+variable "mylist" {
+  type    = list(string)
+  default = ["value1", "value2", "value3"]
+}
+
+variable "mymap" {
+  type = map(string)
+  default = {
+    key1 = "value1"
+    key2 = "value2"
+    key3 = "value3"
+  }
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = var.vpc_name
+    Key1 = var.mylist[0]
+    Key2 = var.mymap["key1"]
+  }
+}
+
